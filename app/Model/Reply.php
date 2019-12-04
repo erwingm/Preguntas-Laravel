3d9246955fbcf33f6e0b642cb0f27443f8f826f5
ErwinGM <erwin.guerra.mamani@gmail.com>
2019-12-04 17:41:58 -0500

    Relaciones de Tablas

--- app/Model/Reply.php
+++ app/Model/Reply.php
@@ -7,4 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 class Reply extends Model
 {
     //
+    public function question(){
+        return $this->belongTo(Question::class);
+    }
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+    public function like(){
+        return $this->hasMany(Like::class);
+    }
 }
