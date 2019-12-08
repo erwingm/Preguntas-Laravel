dff1beb9d88a35d77b27113cb05ffe764f7b6709
ErwinGM <erwin.guerra.mamani@gmail.com>
2019-12-07 20:10:50 -0500

    QuestionResource CRUD

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use App\Model\Question;
+
 use Illuminate\Http\Request;
+use App\Model\Question;
+use App\Http\Resources\QuestionResource;
 
 class QuestionController extends Controller
 {
@@ -15,16 +17,7 @@ class QuestionController extends Controller
     public function index()
     {
         //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
+        return QuestionResource::collection(Question::latest()->get());
     }
 
     /**
@@ -36,6 +29,12 @@ class QuestionController extends Controller
     public function store(Request $request)
     {
         //
+        // auth()->user()->question()->create($request->all());
+        Question::create($request->all());
+        return response('created',Response::HTTP_CREATE);
+
+      
+
     }
 
     /**
@@ -47,18 +46,9 @@ class QuestionController extends Controller
     public function show(Question $question)
     {
         //
+        return new QuestionResource($question);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Model\Question  $question
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Question $question)
-    {
-        //
-    }
 
     /**
      * Update the specified resource in storage.
@@ -81,5 +71,7 @@ class QuestionController extends Controller
     public function destroy(Question $question)
     {
         //
+        $question->delete();
+        return response('Eliminado',201);
     }
 }
--- app/Model/Question.php
+++ app/Model/Question.php
@@ -3,16 +3,34 @@
 namespace App\Model;
 
 use Illuminate\Database\Eloquent\Model;
+use App\User;
+use App\Model\Reply;
+use App\Model\Category;
 
 class Question extends Model
 {
+    public function getRouteKeyName(){
+        //se cambia en la forma de obtener el id -> ahora id = slug (si es que se quiere buscar se debe de hacer por el slug)
+        return 'slug';
+    }
+    // protected $fillable= ['title','slug','body','category_id','user_id'];
+
+    //reemplaza al fillable
+    protected $guarded=[];
+
     public function user(){
-        return $this->belongTo(User::class);
+        return $this->belongsTo(User::class);
     }
     public function replies(){
         return $this->hasMany(Reply::class);
     }
     public function category(){
         return $this->belongsTo(Category::class);
+    }
+
+    public function getPathAttribute(){
+
+        return asset("api/question/$this->slug");
+
     }
 }
